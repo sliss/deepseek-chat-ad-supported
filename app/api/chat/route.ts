0849139ea23b58763b36d6a514583df30afc14ec
Steven Liss <stevenliss45@gmail.com>
2025-02-12 14:20:12 -0500

    reduce max api timeout 300->30

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,14 +2,15 @@ import { deepseek } from '@ai-sdk/deepseek';
 import { fireworks } from '@ai-sdk/fireworks';
 import { streamText } from 'ai';
 
-export const maxDuration = 300;
+// Update maxDuration to 30 seconds for hobby plan
+export const maxDuration = 30;
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = streamText({
     model: fireworks('accounts/fireworks/models/deepseek-v3'),
-    system: "You are Mr. Meeseeks, a helpful assistant that takes in the web for information and replies to the user with correct answer. Remember, thinking is uncomfortable, so try to think as little as possible.",
+    system: "You are a helpful assistant that takes in the web for information and replies to the user with correct answer.",
     messages,
   });
 
